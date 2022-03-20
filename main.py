a78737d9c5e40ec5892098d37da3b58909da2046
Артём <snowboard_team@mail.ru>
2022-03-20 13:01:15 +0600

    Finish hw

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from utils1 import *
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+data = load_candidates_from_json("candidates.json")
 
-@app.route("/<int:id>")
-def page_main(id):
-    return render_template("list.html", candidates=get_candidate(id))
 
-#
-# @app.route("/candidate/<int:id>")
-# def page_candidate(id):
-#     return get_candidate_id(id, all_candidates)
-#
-#
-# @app.route("/skills/<skill>")
-# def page_skills(skill):
-#     return get_candidate_skills(skill, all_candidates)
-#
-# @app.route("/sanya")
-# def sanek():
-#     return "Привет Саня!)"
+@app.route("/")
+def page_main():
+    return render_template("list.html", candidates=data)
+
+
+@app.route("/candidate/<int:id>")
+def page_candidate(id):
+    candidate = get_candidate(id)
+    return render_template("single.html", candidate=candidate)
+
+
+@app.route("/search/<candidate_name>")
+def page_search_name(candidate_name):
+    candidates = get_candidates_by_name(candidate_name)
+    return render_template("search.html", candidates=candidates, candidates_len=len(candidates))
+
+
+@app.route("/skills/<skill>")
+def page_skills(skill):
+    candidates = get_candidates_by_skill(skill)
+    return render_template("skills.html", candidates=candidates, candidates_len=len(candidates), skill=skill)
+
 
 if __name__ == '__main__':
     app.run()
-
--- utils1.py
+++ utils1.py
@@ -1,44 +1,43 @@
 import json
 
+__data = []
+
 
 def load_candidates_from_json(path):
     """Функция загружает весь список из Json файла"""
-
+    global __data
     file = open(path, "r", encoding="utf - 8")
-    data = json.load(file)
+    __data = json.load(file)
     file.close()
-    return data
-
-
-candidates = load_candidates_from_json("candidates.json")
+    return __data
 
 
 def get_candidate(candidate_id):
     """Возвращает одного кандидата по его id"""
-    return candidates[candidate_id-1]
+    for candidate in __data:
+        if candidate["id"] == candidate_id:
+            return {
+                "name": candidate["name"],
+                "position": candidate["position"],
+                "picture": candidate["picture"],
+                "skills": candidate["skills"]
+            }
+    return {'not_found': 'Кандидат не найден'}
 
 
-
-def get_candidates_by_name(candidate_name, candidates):
+def get_candidates_by_name(candidate_name):
     """Возвращает кандидатов по имени"""
-    for candidate in candidates:
-        if candidate_name == candidate["name"]:
-            return candidate_name
+    # for candidate in __data:
+    #     if candidate_name == candidate["name"]:
+    #         return candidate_name
+    return [candidate for candidate in __data if candidate_name.lower() in candidate["name"].lower()]
 
 
-
-
-def get_candidates_by_skill(skill_name, candidates):
+def get_candidates_by_skill(skill_name):
     """Возвращает кандидатов по навыку"""
-    all_candidate_skill = ""
-    for candidate in candidates:
-        skill_list = candidate["skills"].lower().split(",")
+    all_candidate_skill = []
+    for candidate in __data:
+        skill_list = candidate["skills"].lower().split(", ")
         if skill_name.lower() in skill_list:
-            candidate_user = candidate["skills"]
-            all_candidate_skill += candidate_user
-
-    if len(all_candidate_skill) < 1:
-        return "Такой навык не найден"
-    else:
-        return all_candidate_skill
-
+            all_candidate_skill.append(candidate)
+    return all_candidate_skill
